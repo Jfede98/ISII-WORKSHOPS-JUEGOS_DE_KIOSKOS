758a3d0ab20911c46005188cc02e39897359713b
Julian Riera <jfriera@espol.edu.ec>
2022-01-21 17:54:47 -0500

    Test plan completed

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/CarInsuranceTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/CarInsuranceTest.java
@@ -11,6 +11,124 @@ import org.junit.Test;
 public class CarInsuranceTest {
 	
 	/*This class tests the CarInsurance class*/
+	@Test 
+	public void calculateInsuranceTC1() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("20", "M" , "false", "true");
+		int expectedInsurance = 2000;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
 	
+	@Test 
+	public void calculateInsuranceTC2() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("26", "M" , "false", "true");
+		int expectedInsurance = 500;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC3() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("50", "M" , "false", "true");
+		int expectedInsurance = 400;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC4() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("50", "M" , "true", "true");
+		int expectedInsurance = 100;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC5() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("18", "F" , "false", "true");
+		int expectedInsurance = 300;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC6() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("31", "F" , "true", "true");
+		int expectedInsurance = 200;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC7() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("61", "F" , "false", "true");
+		int expectedInsurance = 150;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC8() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("17", "F" , "true", "true");
+		int expectedInsurance = -1;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC9() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("83", "M" , "true", "true");
+		int expectedInsurance = -1;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC10() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("-1", "M" , "true", "true");
+		int expectedInsurance = -1;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC11() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("27", "O" , "true", "true");
+		int expectedInsurance = -1;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
+	
+	@Test 
+	public void calculateInsuranceTC12() {
+		
+		CarInsurance tc = new CarInsurance();
+		int insurance = tc.getPremium("83", "F" , "true", "false");
+		int expectedInsurance = -1;
+		Assert.assertEquals(insurance, expectedInsurance, 0);
+		
+	}
 	
 }
